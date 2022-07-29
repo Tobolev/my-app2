3ecc56f06fc441522a8a88b22d4bf2fa100b1a26
Tobolev <monarchwow@gmail.com>
2022-07-29 17:07:58 +0300

    Chapter 3 Lesson 35

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,16 +1,26 @@
 import './CostItem.css';
 import CostDate from './CostDate';
 import Card from '../UI/Card';
+import React, { useState } from 'react';
 
 const CostItem = (props) => {
+    const [costName, setCostName] = useState(props.costName);
+
+    const changeCostNameHandler = () => {
+        
+        setCostName('New cost');
+        console.log(costName);
+    };
 
     return (
+
     <Card className='cost-item'>
     <CostDate costDate={props.costDate}/>
     <div className='cost-item__description'>
-        <h2>{props.costName}</h2>
+        <h2>{costName}</h2>
         <div className='cost-item__price'>${props.costAmount}</div>
     </div>
+    <button onClick={changeCostNameHandler}>Изменить описание</button>
     </Card>
     );
 }
